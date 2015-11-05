8b9511f15fc57c5139e344fb8f602253bd6cda67
Gaurav Yadav <gauravshelangia@gmail.com>
2015-11-05 11:25:47 +0530

    C++ program for summation of all array element

---
+++ Algorithms/Warmup/Simple_Array_sum.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    scanf("%d",&n);
+    int sum[n],s=0;
+    for(int i=0;i<n;i++){
+        scanf("%d",&sum[i]);
+        s = s+sum[i];
+    }
+   printf("%d",s);
+    return 0;
+}
