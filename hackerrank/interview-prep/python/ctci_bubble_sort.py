304b449633b44038f26313e0824c22ff85a93570
Gaurav <gaurav.yadav@webklipper.com>
2019-11-19 08:07:34 +0530

    new problems

---
+++ hackerrank/interview-prep/python/ctci_bubble_sort.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countSwaps function below.
+def countSwaps(a):
+    n = len(a)
+    num_swaps = 0;
+    for i in range(0,n):
+        for j in range(0, n-1):
+            if (a[j] > a[j + 1]):
+                a[j], a[j + 1] = a[j+1], a[j]
+                num_swaps += 1
+    print("Array is sorted in",num_swaps, "swaps.")
+    print("First Element:",a[0])
+    print("Last Element:", a[-1])
+
+if __name__ == '__main__':
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
+
